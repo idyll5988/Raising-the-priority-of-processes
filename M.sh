f399f2950c712dca671878d5db049ff65f7cf03b
idyll <liushun968858@gmail.com>
2024-08-19 17:46:47 +0800

    Add files via upload

--- M.sh
+++ M.sh
@@ -3,5 +3,5 @@
 MODPATH="/data/adb/modules/priority"
 [[ ! -e ${MODDIR}/ll/log ]] && mkdir -p ${MODDIR}/ll/log
 screen_status=$(dumpsys window | grep "mScreenOn" | grep true)
-while true;do if [[ "$screen_status" ]]; then for a in $(dumpsys activity|grep mResumed|cut -f8 -d ' '|cut -f1 -d/);do am send-trim-memory $a RUNNING_CRITICAL;chrt -r -p 99 $(pidof $a);done; >/dev/null 2>&1 &
+while true;do if [[ "$screen_status" ]]; then;for a in $(dumpsys activity|grep mResumed|cut -f8 -d ' '|cut -f1 -d/);do am send-trim-memory $a RUNNING_CRITICAL;chrt -r -p 99 $(pidof $a);done; >/dev/null 2>&1 &
 fi	
